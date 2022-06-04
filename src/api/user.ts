2933757a9cacdc7d624a41b571d0bb7961378b81
Takuya Nishio <littlecub240@gmail.com>
2022-06-04 12:23:47 +0900

    add form sample code

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { Axios } from "axios";
 
 export type UserResponse =  {
   login: string,
@@ -9,10 +9,28 @@ export type fetchWebApiParams = {
   user: string
 }
 
+type SubmitWebApiParams = {
+  id: number | null,
+  content: string | null
+}
+
+type UpdateWebApiParams = {
+  id: number
+  content: string
+}
+
 export async function fetchWebApi(params: fetchWebApiParams): Promise<UserResponse | null> {
   const response = await axios.get(`https://api.github.com/users/${params.user}`)
   return {
     login: response.data.login,
     url: response.data.url
   }
+}
+export function submitWebApi(params: SubmitWebApiParams): Promise<Axios> {
+  console.log(params)
+  return axios.get(`https://api.github.com/users/webuilder240`)
+}
+export function updateWebApi(params: UpdateWebApiParams): Promise<Axios> {
+  console.log(params)
+  return axios.get(`https://api.github.com/users/webuilder240`)
 }
--- src/components/FetchUser.vue
+++ src/components/FetchUser.vue
@@ -10,27 +10,66 @@
       <input type="text" v-model="userName">
       <button @click="fetchUser"> reFetch User </button>
     </div>
+
+    <div>
+      <input type="text" v-model="formData.content">
+      <button @click="submit"> Submit!!! </button>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import { fetchWebApi, UserResponse } from '@/api/user'
-type UserData = {
+import { fetchWebApi, submitWebApi, updateWebApi, UserResponse } from '@/api/user'
+
+type Form = {
+  id: number | null,
+  content: string | null
+}
+type Data = {
   user: UserResponse | null,
-  userName: string
+  userName: string, 
+  formData: Form
 }
 export default Vue.extend({
-  data(): UserData {
+  data(): Data {
     return {
       user: null,
       userName: "webuilder240",
+      formData: {
+        id: null, 
+        content: null
+      }
     }
   },
   created() {
     this.fetchUser()
+    this.setId()
   },
   methods: {
+    setId() {
+      const url = new URL(location.href)
+      const id = url.searchParams.get("id")
+      if (id) {
+        this.formData.id = parseInt(id)
+      }
+    },
+    async submit() {
+      try {
+        await submitWebApi(this.formData)
+      } catch(e) {
+        console.error(e)
+      }
+    },
+    async updateSubmit() {
+      try {
+        if (this.formData.id && this.formData.content) {
+          await updateWebApi({id: this.formData.id, content: this.formData.content})
+        }
+      } catch(e) {
+        console.error(e)
+      }
+    },
     async fetchUser() {
       try {
         const response = await fetchWebApi({user: this.userName})
